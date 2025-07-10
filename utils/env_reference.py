d55c8093de7de2c21d292648d09895abb76489a9
ozmanda <usher.juliapatricia@gmail.com>
2025-07-10 16:44:54 +0200

    added rail and switch clusters

--- utils/env_reference.py
+++ utils/env_reference.py
@@ -1,5 +1,12 @@
+import numpy as np
 from flatland.envs.rail_env import RailEnv
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Dict, Tuple, Any, Union
+
+import networkx as nx
+from networkx import DiGraph
+from utils.flatland_railway_extension.RailroadSwitchAnalyser import RailroadSwitchAnalyser
+from utils.flatland_railway_extension.RailroadSwitchCluster import RailroadSwitchCluster
+from utils.flatland_railway_extension.FlatlandGraphBuilder import FlatlandGraphBuilder
 
 class Transition(NamedTuple):
     state: dict
@@ -14,17 +21,17 @@ class EnvReference():
         self.env: RailEnv = env
 
     def get_agent_handles(self):
-        """Get the agent handles from the current environment."""
+        """ Get the agent handles from the current environment. """
         
 
     def get_environment_info(self) -> dict:
-        """Get information about the current environment."""
+        """ Get information about the current environment. """
         if self.env:
             return {'info 1': 'value1', 'info 2': 'value2'}  # Example info, replace with actual logic
         return {}
 
-    def get_metrics(self) -> dict:
-        """Get evaluation metrics from the current environment."""
+    def get_metrics(self) -> Dict[str, Any]:
+        """ Get evaluation metrics from the current environment. """
         if self.env:
             metrics = {}
             metrics['total_agents'] = len(self.env.agents)
@@ -44,29 +51,57 @@ class FlatlandEnvReference(EnvReference):
         self.state: dict = {}
         self.info: dict = {}
         self.next_state: dict = {}
+        self.network_graph: Union[DiGraph, None] = None
     
-    def init_environment(self, env: RailEnv = None):
+    def init_environment(self, env: RailEnv = None) -> None:
         if not self.env:
             self.env = env 
         state, info = self.env.reset()
         self.state = state
         self.info = info
         self.transitions: List[Transition] = []
+        self._init_clusters()
+        self._init_network_graph()
 
-    def get_agent_handles(self):
-        """Get the agent handles from the Flatland environment."""
+    def _init_clusters(self) -> None: 
+        """ Initialize the switch and railroad clusters for the Flatland environment. """
+        if self.env:
+            # Initialize grids for rail and switch clusters
+            self.rail_grid: np.ndarray = np.zeros((self.env.height, self.env.width), dtype=np.int8)
+            self.switch_grid: np.ndarray = np.zeros((self.env.height, self.env.width), dtype=np.int8)
+
+            self.switch_analyser = RailroadSwitchAnalyser(self.env)
+            self.switch_cluster = RailroadSwitchCluster(self.switch_analyser)
+
+            for id, positions in self.switch_cluster.connecting_edge_clusters.items():
+                for pos in positions:
+                    self.rail_grid[pos[0], pos[1]] = id
+            
+            for id, positions in self.switch_cluster.railroad_switch_clusters.items():
+                for pos in positions:
+                    self.switch_grid[pos[0], pos[1]] = id
+
+
+    def _init_network_graph(self) -> None:
+        """ Initialize the network graph for the Flatland environment. """
+        if self.env:
+            graphbuilder = FlatlandGraphBuilder(self.switch_analyser, activate_simplified=True)
+            self.network_graph = graphbuilder.get_graph()
+
+    def get_agent_handles(self) -> List[int | str]:
+        """ Get the agent handles from the Flatland environment. """
         if self.env:
                 return self.env.get_agent_handles()
         return []
     
-    def get_environment_info(self) -> dict:
-        """Get information about the Flatland environment."""
+    def get_environment_info(self) -> Dict[str, Any]:
+        """ Get information about the Flatland environment. """
         if self.env:
             return {'info 1': 'value1', 'info 2': 'value2'}  # Example info, replace with actual logic
         return {}
     
-    def get_metrics(self) -> dict:
-        """Get evaluation metrics from the Flatland environment."""
+    def get_metrics(self) -> Dict[str, Any]:
+        """ Get evaluation metrics from the Flatland environment. """
         if self.env:
             metrics = {}
             metrics['total_agents'] = len(self.env.agents)
@@ -78,24 +113,22 @@ class FlatlandEnvReference(EnvReference):
         return {}
     
 
-    def step_environment(self, action_dict):
-        """Step the Flatland environment with the given action dictionary."""
+    def step_environment(self, action_dict) -> None:
+        """ Step the Flatland environment with the given action dictionary. """
         if self.env:
-            next_state, rewards, dones, infos = self.env.step(action_dict)
+            next_state, rewards, dones, _ = self.env.step(action_dict)
             transition = Transition(
-                state=self.env.get_state(),
+                state=self.env._get_observations(),
                 action=action_dict,
                 reward=rewards,
                 next_state=next_state,
                 done=dones
             )
             self.transitions.append(transition)
-            return 
-        return None
     
     
-    def reset_environment(self):
-        """Reset the Flatland environment."""
+    def reset_environment(self) -> Tuple:
+        """ Reset the Flatland environment. """
         if self.env:
             self.state, self.info = self.env.reset()
             self.transitions.clear()
